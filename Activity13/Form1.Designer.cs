1d19e6fee10246d5ae5ff47a74a50799f9c415bb
JordanHayen <jordan.hayen@gmail.com>
2021-10-30 11:10:30 -0700

    The files for Activity 13

---
+++ Activity13/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Activity13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UpperLeft = new System.Windows.Forms.Label();
+            this.UpperCenter = new System.Windows.Forms.Label();
+            this.UpperRight = new System.Windows.Forms.Label();
+            this.CenterLeft = new System.Windows.Forms.Label();
+            this.Center = new System.Windows.Forms.Label();
+            this.CenterRight = new System.Windows.Forms.Label();
+            this.LowerLeft = new System.Windows.Forms.Label();
+            this.LowerCenter = new System.Windows.Forms.Label();
+            this.LowerRight = new System.Windows.Forms.Label();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.NewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UpperLeft
+            // 
+            this.UpperLeft.AutoSize = true;
+            this.UpperLeft.BackColor = System.Drawing.SystemColors.Control;
+            this.UpperLeft.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.UpperLeft.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.UpperLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.UpperLeft.Location = new System.Drawing.Point(73, 50);
+            this.UpperLeft.Name = "UpperLeft";
+            this.UpperLeft.Size = new System.Drawing.Size(80, 78);
+            this.UpperLeft.TabIndex = 0;
+            this.UpperLeft.Text = "X";
+            this.UpperLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UpperCenter
+            // 
+            this.UpperCenter.AutoSize = true;
+            this.UpperCenter.BackColor = System.Drawing.SystemColors.Control;
+            this.UpperCenter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.UpperCenter.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.UpperCenter.Location = new System.Drawing.Point(157, 50);
+            this.UpperCenter.Name = "UpperCenter";
+            this.UpperCenter.Size = new System.Drawing.Size(80, 78);
+            this.UpperCenter.TabIndex = 1;
+            this.UpperCenter.Text = "X";
+            this.UpperCenter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UpperRight
+            // 
+            this.UpperRight.AutoSize = true;
+            this.UpperRight.BackColor = System.Drawing.SystemColors.Control;
+            this.UpperRight.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.UpperRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.UpperRight.Location = new System.Drawing.Point(241, 50);
+            this.UpperRight.Name = "UpperRight";
+            this.UpperRight.Size = new System.Drawing.Size(80, 78);
+            this.UpperRight.TabIndex = 2;
+            this.UpperRight.Text = "X";
+            this.UpperRight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CenterLeft
+            // 
+            this.CenterLeft.AutoSize = true;
+            this.CenterLeft.BackColor = System.Drawing.SystemColors.Control;
+            this.CenterLeft.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.CenterLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.CenterLeft.Location = new System.Drawing.Point(73, 126);
+            this.CenterLeft.Name = "CenterLeft";
+            this.CenterLeft.Size = new System.Drawing.Size(80, 78);
+            this.CenterLeft.TabIndex = 3;
+            this.CenterLeft.Text = "X";
+            this.CenterLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Center
+            // 
+            this.Center.AutoSize = true;
+            this.Center.BackColor = System.Drawing.SystemColors.Control;
+            this.Center.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Center.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.Center.Location = new System.Drawing.Point(157, 126);
+            this.Center.Name = "Center";
+            this.Center.Size = new System.Drawing.Size(80, 78);
+            this.Center.TabIndex = 4;
+            this.Center.Text = "X";
+            this.Center.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CenterRight
+            // 
+            this.CenterRight.AutoSize = true;
+            this.CenterRight.BackColor = System.Drawing.SystemColors.Control;
+            this.CenterRight.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.CenterRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.CenterRight.Location = new System.Drawing.Point(241, 126);
+            this.CenterRight.Name = "CenterRight";
+            this.CenterRight.Size = new System.Drawing.Size(80, 78);
+            this.CenterRight.TabIndex = 5;
+            this.CenterRight.Text = "X";
+            this.CenterRight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LowerLeft
+            // 
+            this.LowerLeft.AutoSize = true;
+            this.LowerLeft.BackColor = System.Drawing.SystemColors.Control;
+            this.LowerLeft.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LowerLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.LowerLeft.Location = new System.Drawing.Point(73, 202);
+            this.LowerLeft.Name = "LowerLeft";
+            this.LowerLeft.Size = new System.Drawing.Size(80, 78);
+            this.LowerLeft.TabIndex = 6;
+            this.LowerLeft.Text = "X";
+            this.LowerLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LowerCenter
+            // 
+            this.LowerCenter.AutoSize = true;
+            this.LowerCenter.BackColor = System.Drawing.SystemColors.Control;
+            this.LowerCenter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LowerCenter.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.LowerCenter.Location = new System.Drawing.Point(157, 202);
+            this.LowerCenter.Name = "LowerCenter";
+            this.LowerCenter.Size = new System.Drawing.Size(80, 78);
+            this.LowerCenter.TabIndex = 7;
+            this.LowerCenter.Text = "X";
+            this.LowerCenter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LowerRight
+            // 
+            this.LowerRight.AutoSize = true;
+            this.LowerRight.BackColor = System.Drawing.SystemColors.Control;
+            this.LowerRight.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LowerRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.LowerRight.Location = new System.Drawing.Point(241, 202);
+            this.LowerRight.Name = "LowerRight";
+            this.LowerRight.Size = new System.Drawing.Size(80, 78);
+            this.LowerRight.TabIndex = 8;
+            this.LowerRight.Text = "X";
+            this.LowerRight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ResultLabel.Location = new System.Drawing.Point(73, 291);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(248, 20);
+            this.ResultLabel.TabIndex = 9;
+            this.ResultLabel.Text = "X Wins";
+            this.ResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // NewGame
+            // 
+            this.NewGame.BackColor = System.Drawing.SystemColors.Control;
+            this.NewGame.Location = new System.Drawing.Point(73, 314);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(248, 27);
+            this.NewGame.TabIndex = 10;
+            this.NewGame.Text = "New Game";
+            this.NewGame.UseVisualStyleBackColor = false;
+            this.NewGame.Click += new System.EventHandler(this.NewGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(395, 409);
+            this.Controls.Add(this.NewGame);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.LowerRight);
+            this.Controls.Add(this.LowerCenter);
+            this.Controls.Add(this.LowerLeft);
+            this.Controls.Add(this.CenterRight);
+            this.Controls.Add(this.Center);
+            this.Controls.Add(this.CenterLeft);
+            this.Controls.Add(this.UpperRight);
+            this.Controls.Add(this.UpperCenter);
+            this.Controls.Add(this.UpperLeft);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UpperLeft;
+        private System.Windows.Forms.Label UpperCenter;
+        private System.Windows.Forms.Label UpperRight;
+        private System.Windows.Forms.Label CenterLeft;
+        private System.Windows.Forms.Label Center;
+        private System.Windows.Forms.Label CenterRight;
+        private System.Windows.Forms.Label LowerLeft;
+        private System.Windows.Forms.Label LowerCenter;
+        private System.Windows.Forms.Label LowerRight;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button NewGame;
+    }
+}
+
---
+++ Activity13/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Activity13
+{
+    public partial class Form1 : Form
+    {
+
+        private char[,] board; // A two-dimensional array of characters that will represent the board. Made with help from the textbook (Gaddis, 445)
+        private char result; // A string that will contain the result of the game
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // This code runs when the NewGame button is clicked
+        private async void NewGame_Click(object sender, EventArgs e)
+        {
+            ClearBoard(); // The ClearBoard method is called
+            await Task.Delay(1500); // The code is delayed for 1.5 seconds. Made with help from Microsoft documentation (n.d.)
+
+            while (!GameOver(out result)) // This loop runs until the game is over. The letters X and O are played in turn until the game is over
+            {
+                if(!GameOver(out result)) // If the game is not over, the letter X is played. If the game is over, the result variable is set to the winning letter
+                {
+                    ResultLabel.Text = "X playing..."; // The text of the ResultLabel is set to notify the user that X is playing
+                    PlayLetter('X'); // The PlayLetter method is called with the letter X as its parameter
+                }
+
+                await Task.Delay(1500); // The code is delayed for 1.5 seconds. Made with help from Microsoft documentation (n.d.)
+                DisplayBoard(); // Displays the board in the GUI
+
+                if (!GameOver(out result)) // If the game is not over, the letter O is played. If the game is over, the result variable is set to the winning letter
+                {
+                    ResultLabel.Text = "O playing..."; // The text of the ResultLabel is set to notify the user that O is playing
+                    PlayLetter('O'); // The PlayLetter method is called with the letter O as its parameter
+                }
+
+                await Task.Delay(1500); // The code is delayed for 1.5 seconds. Made with help from Microsoft documentation (n.d.)
+                DisplayBoard(); // Displays the board in the GUI
+            }
+
+            if (result != ' ') // If the result variable does not contain an empty space the text of the ResultLabel is set to to a string that contains the winning letter
+            {
+                ResultLabel.Text = result.ToString() + " wins.";
+            }
+            else // If the result variable does contain an empty space the text of the ResultLabel is set to a string telling the user that the result was a tie
+            {
+                ResultLabel.Text = "The game is a tie.";
+            }
+
+            DisplayBoard(); // Displays the board in the GUI
+        }
+
+        // This method displays the board in the GUI
+        private void DisplayBoard()
+        {
+            // The text of the labels that represent the upper row are set to the corresponding items in the board array
+            UpperLeft.Text = board[0,0].ToString();
+            UpperCenter.Text = board[0, 1].ToString();
+            UpperRight.Text = board[0, 2].ToString();
+
+            // The text of the labels that represent the center row are set to the corresponding items in the board array
+            CenterLeft.Text = board[1, 0].ToString();
+            Center.Text = board[1, 1].ToString();
+            CenterRight.Text = board[1, 2].ToString();
+
+            // The text of the labels that represent the lower row are set to the corresponding items in the board array
+            LowerLeft.Text = board[2, 0].ToString();
+            LowerCenter.Text = board[2, 1].ToString();
+            LowerRight.Text = board[2, 2].ToString();
+        }
+
+        // This method determines whether or not the game is over. Made with help from the textbook (Gaddis, 367)
+        private bool GameOver(out char letter)
+        {
+            
+            if (board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2] && board[0,2] != ' ') // Checks if the the upper row contains all the same characters and if that character is not a space
+            {
+                letter = board[0,0]; // The output parameter is set to the winning character
+                return true;
+            }
+            else if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2] && board[1, 2] != ' ') // Checks if the the middle row contains all the same characters and if that character is not a space
+            {
+                letter = board[1, 0]; // The output parameter is set to the winning character
+                return true;
+            }
+            else if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2] && board[2, 2] != ' ') // Checks if the the lower row contains all the same characters and if that character is not a space
+            {
+                letter = board[2, 0]; // The output parameter is set to the winning character
+                return true;
+            }
+            else if (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0] && board[2, 0] != ' ') // Checks if the the left column contains all the same characters and if that character is not a space
+            {
+                letter = board[0, 0]; // The output parameter is set to the winning character
+                return true;
+            }
+            else if (board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1] && board[2, 1] != ' ') // Checks if the the center column contains all the same characters and if that character is not a space
+            {
+                letter = board[0, 1]; // The output parameter is set to the winning character
+                return true;
+            }
+            else if (board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2] && board[2, 2] != ' ') // Checks if the the right column contains all the same characters and if that character is not a space
+            {
+                letter = board[0, 2]; // The output parameter is set to the winning character
+                return true;
+            }
+            else if(board[0,0] == board[1,1] && board[1,1] == board[2,2] && board[2, 2] != ' ') // Checks if the the diagonal from the upper-left corner to the lower-right contains all the same characters and if that character is not a space
+            {
+                letter = board[0, 0]; // The output parameter is set to the winning character
+                return true;
+            }
+            else if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[2, 0] != ' ') // Checks if the the diagonal from the upper-right corner to the lower-left contains all the same characters and if that character is not a space
+            {
+                letter = board[0, 2]; // The output parameter is set to the winning character
+                return true;
+            }
+            else if(IsTie()) // Checks if the result is a tie. The logic of determining a tie is much different than that for determining a win. Therefore, a method is dedicated to it
+            {
+                letter = ' '; // The output parameter is set to a space
+                return true;
+            }
+            else // If the result is not a tie or win, then the game is not over
+            {
+                letter = ' '; // The output parameter is set to a space
+                return false;
+            }
+
+        }
+
+        // This method checks if the game is tied
+        private bool IsTie()
+        {
+            for (int i = 0; i < 3; i++) // This nested loop checks every item in the board array. If any item is an empty space then the board is not full and the game is not tied
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == ' ')
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true; // If none of the items in the array are an empty space then the board is full and the game is a tie
+        }
+
+        // This method adds a letter (an X or an O) to the board
+        private void PlayLetter(char letter)
+        {
+            Random randomGenerator1 = new Random(); // Creates and initializes a new Random object
+            Random randomGenerator2 = new Random(randomGenerator1.Next()); // Creates and initializes a new Random object. By default, Random objects have as their seed the time at which they are created. To prevent the two Random objects from having the same seed, the second uses a number generated by the first as a seed
+            int[] cell = new int[2] { randomGenerator1.Next(3), randomGenerator2.Next(3) }; // Creates and initializes an array of integers with two items. This array will represent a random cell in the board. It is initialized with two random values generated by the above Random objects 
+
+            while(board[cell[0], cell[1]] != ' ') // This loop randomly selects cells in the board until an empty cell is found
+            {
+                cell[0] = randomGenerator1.Next(3);
+                cell[1] = randomGenerator2.Next(3);
+            }
+
+            board[cell[0], cell[1]] = letter; // Adds the letter to the cell
+
+        }
+
+        // This method sets the board array to contain empty spaces and then displays the board, effectively clearing the board
+        private void ClearBoard()
+        {
+            board = new char[,]
+            {
+                { ' ', ' ', ' ' },
+                { ' ', ' ', ' ' },
+                { ' ', ' ', ' ' }
+            }; // Initializes the board array as a three by three array of empty spaces. Made with help from the textbook (Gaddis, 447)
+            ResultLabel.Text = " "; // Clears the text of the ResultLabel
+
+            DisplayBoard(); // Displays the board in the GUI
+        }
+    }
+}
+
+// References:
+// Gaddis, T. (2020). Starting Out With Visual C#. Pearson.
+// Microsoft. (n.d.). Task.Delay Method. https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.delay?view=net-5.0
